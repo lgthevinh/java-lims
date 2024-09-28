16aed3726d0c846f730a95417f9d11d136ca412d
lgthvinh <everwellmax@gmail.com>
2024-09-28 23:39:03 +0700

    test: configuring JavaFX

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Java Library Management System");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Stage primaryStage = new Stage();
+        Group root = new Group();
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
