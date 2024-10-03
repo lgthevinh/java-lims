8c1757f2499e0e9169d1cc996662d557a69cd34c
lgthvinh <everwellmax@gmail.com>
2024-10-04 00:15:08 +0700

    add: static functions for Book entity (getAll, getByISBN, add)
    Signed-off-by: lgthvinh <everwellmax@gmail.com>

--- src/main/java/java_lims/model/DatabaseManager.java
+++ src/main/java/java_lims/model/DatabaseManager.java
@@ -1,13 +1,71 @@
 package java_lims.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseManager {
     private static final String DB_URL = "jdbc:sqlite:./src/main/resources/database/db.sqlite";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL);
+    }
+
+    public static List<Book> getAllBooks() throws SQLException {
+        List<Book> books = new ArrayList<>();
+        Connection conn = getConnection();
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM Book LIMIT 100");
+
+        while (resultSet.next()) {
+            Book book = new Book(
+                    resultSet.getInt("isbn"),
+                    resultSet.getString("title"),
+                    resultSet.getString("author"),
+                    resultSet.getDate("year_of_publication"),
+                    resultSet.getString("publisher"),
+                    resultSet.getString("image_url"),
+                    resultSet.getInt("available_amount")
+            );
+            books.add(book);
+        }
+        conn.close();
+        return books;
+    }
+
+    public static Book getBookByISBN(Integer isbn) throws SQLException {
+        Connection conn = getConnection();
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM Book WHERE isbn = " + isbn);
+
+        if (resultSet.first()) {
+            conn.close();
+            return new Book(
+                    resultSet.getInt("isbn"),
+                    resultSet.getString("title"),
+                    resultSet.getString("author"),
+                    resultSet.getDate("year_of_publication"),
+                    resultSet.getString("publisher"),
+                    resultSet.getString("image_url"),
+                    resultSet.getInt("available_amount")
+            );
+        }
+        conn.close();
+        return null;
+    }
+
+    public static void addBookToDatabase(Book book) throws SQLException {
+        Connection conn = getConnection();
+        Statement statement = conn.createStatement();
+        statement.executeUpdate("INSERT INTO Book VALUES (%d, %s, %s, %s, %s, %s, %d)".formatted(
+                (Integer) book.getIsbn(),
+                book.getTitle(),
+                book.getAuthor(),
+                book.getYearOfPublication(),
+                book.getPublisher(),
+                book.getImageUrl(),
+                book.getAvailableAmount()
+        ));
+        conn.close();
     }
 }
