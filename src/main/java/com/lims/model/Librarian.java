220a50d5b34a04f7f2935e17266a32c4ce25dac9
lgthvinh <everwellmax@gmail.com>
2024-10-06 01:54:52 +0700

    update: Librarian object
    Signed-off-by: lgthvinh <everwellmax@gmail.com>

--- src/main/java/com/lims/model/Librarian.java
+++ src/main/java/com/lims/model/Librarian.java
@@ -3,37 +3,17 @@ package com.lims.model;
 import java.util.Date;
 
 public class Librarian extends User {
-    private Integer emp_id;
-    private Date start_date;
-    private Date end_date;
+    private Integer empId;
 
-    public Librarian(Integer emp_id, Integer userId, Integer socialId, String name, Date dateOfBirth, String addressLine, String phoneNumber, String email, String password) {
+    public Librarian(Integer userId, Integer socialId, String name, Date dateOfBirth, String addressLine, String phoneNumber, String email, String password) {
         super(userId, socialId, name, dateOfBirth, addressLine, phoneNumber, email, password);
-        this.start_date = new Date();
-        this.emp_id = emp_id;
     }
 
-    public Integer getEmp_id() {
-        return emp_id;
+    public Integer getEmpId() {
+        return empId;
     }
 
-    public void setEmp_id(Integer emp_id) {
-        this.emp_id = emp_id;
-    }
-
-    public Date getStart_date() {
-        return start_date;
-    }
-
-    public void setStart_date(Date start_date) {
-        this.start_date = start_date;
-    }
-
-    public Date getEnd_date() {
-        return end_date;
-    }
-
-    public void setEnd_date(Date end_date) {
-        this.end_date = end_date;
+    public void setEmpId(Integer empId) {
+        this.empId = empId;
     }
 }
