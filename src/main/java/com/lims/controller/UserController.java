e03e2bfebcd3e320c574247e721c1e7c7cf63f60
lgthvinh <everwellmax@gmail.com>
2024-10-06 12:38:41 +0700

    update: interface methods
    Signed-off-by: lgthvinh <everwellmax@gmail.com>

--- src/main/java/com/lims/controller/UserController.java
+++ src/main/java/com/lims/controller/UserController.java
@@ -1,8 +1,19 @@
 package com.lims.controller;
 
 interface UserControllerInterface {
+    boolean isUserLibrarian(Integer userId);
 
+    boolean isUserStudent(Integer userId);
 }
 
 public class UserController implements UserControllerInterface {
+    @Override
+    public boolean isUserLibrarian(Integer userId) {
+        return false;
+    }
+
+    @Override
+    public boolean isUserStudent(Integer userId) {
+        return false;
+    }
 }
