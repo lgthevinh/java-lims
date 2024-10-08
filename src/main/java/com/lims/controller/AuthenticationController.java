98d57909e1cec3791545d0e70388e34adc57d10e
lgthvinh <everwellmax@gmail.com>
2024-10-08 22:39:14 +0700

    define AuthenticationController methods
    Signed-off-by: lgthvinh <everwellmax@gmail.com>

--- src/main/java/com/lims/controller/AuthenticationController.java
+++ src/main/java/com/lims/controller/AuthenticationController.java
@@ -1,7 +1,29 @@
 package com.lims.controller;
 
+import com.lims.model.User;
+
 interface AuthenticationControllerInterface {
+    void authenticateUser(User user);
+
+    void deleteAuthentication();
+
+    boolean isUserAuthenticated(User user);
 }
 
 public class AuthenticationController implements AuthenticationControllerInterface {
+
+    @Override
+    public void authenticateUser(User user) {
+
+    }
+
+    @Override
+    public void deleteAuthentication() {
+
+    }
+
+    @Override
+    public boolean isUserAuthenticated(User user) {
+        return null;
+    }
 }
