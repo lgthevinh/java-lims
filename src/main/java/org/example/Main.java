c463c5816c1a4b9448c5006c9c2d215bb09fc980
lgthvinh <everwellmax@gmail.com>
2024-10-10 08:27:55 +0700

    remove un-nessecity
    Signed-off-by: lgthvinh <everwellmax@gmail.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,34 +1,7 @@
 package org.example;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import view.controller.SplashController;
-
-public class Main extends Application {
+public class Main {
     public static void main(String[] args) {
-        System.setProperty("javafx.preloader", SplashController.class.getName());
-        launch(args);
-    }
-
-    private void simulateLoadingTasks() throws InterruptedException {
-        Thread.sleep(5000);
-    }
-
-    @Override
-    public void init() throws Exception {
-        simulateLoadingTasks();
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/HomeScene.fxml"));
-
-        primaryStage.setTitle("Java Library Management System");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.centerOnScreen();
-        primaryStage.show();
+        System.out.println("Data-core");
     }
 }
