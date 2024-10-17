69bae603295094d4cd55eb60d3d390d74a7de471
lgthvinh <everwellmax@gmail.com>
2024-10-17 22:05:28 +0700

    add overloading constructor to objects
    Signed-off-by: lgthvinh <everwellmax@gmail.com>

--- src/main/java/com/lims/model/Librarian.java
+++ src/main/java/com/lims/model/Librarian.java
@@ -1,11 +1,21 @@
 package com.lims.model;
 
+import java.util.Date;
+
 public class Librarian extends User {
     private Integer empId;
+
+    public Librarian() {
+    }
 
     public Librarian(User user) {
         super(user.getSocialId(), user.getName(), user.getDateOfBirth(), user.getAddressLine(), user.getPhoneNumber(), user.getEmail(), user.getPassword());
         this.setUserId(user.getUserId());
+    }
+
+    public Librarian(String socialId, String name, Date dateOfBirth, String addressLine, String phoneNumber, String email, String password, Integer empId) {
+        super(socialId, name, dateOfBirth, addressLine, phoneNumber, email, password);
+        this.empId = empId;
     }
 
     public Integer getEmpId() {
--- src/main/java/com/lims/model/Student.java
+++ src/main/java/com/lims/model/Student.java
@@ -1,12 +1,24 @@
 package com.lims.model;
 
+import java.util.Date;
+
 public class Student extends User {
     private String studentId;
     private String school;
     private String major;
 
+    public Student() {
+    }
+
     public Student(User user) {
         super(user.getSocialId(), user.getName(), user.getDateOfBirth(), user.getAddressLine(), user.getPhoneNumber(), user.getEmail(), user.getPassword());
+    }
+
+    public Student(String socialId, String name, Date dateOfBirth, String addressLine, String phoneNumber, String email, String password, String studentId, String school, String major) {
+        super(socialId, name, dateOfBirth, addressLine, phoneNumber, email, password);
+        this.studentId = studentId;
+        this.school = school;
+        this.major = major;
     }
 
     public String getStudentId() {
