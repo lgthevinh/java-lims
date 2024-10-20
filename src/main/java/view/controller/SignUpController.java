e879ff867566ec029cf0c19fc2ff336e6b77b6a1
CrisAQ2410 <chuanhquoc2410@gmail.com>
2024-10-20 07:38:48 +0700

    update SignUp

---
+++ src/main/java/view/controller/SignUpController.java
@@ -0,0 +1,29 @@
+package view.controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+
+public class SignUpController {
+
+    public void handleSignInAction(ActionEvent event) {
+        try {
+            // Load the login UI
+            Parent loginRoot = FXMLLoader.load(getClass().getResource("/fxml/LoginUI.fxml"));
+            Scene loginScene = new Scene(loginRoot);
+
+            // Get the current stage
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Set the login scene
+            stage.setScene(loginScene);
+            stage.centerOnScreen();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
