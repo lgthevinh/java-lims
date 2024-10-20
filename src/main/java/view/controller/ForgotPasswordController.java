c317234f1149a38464311b6f42d188d8bca9b02b
CrisAQ2410 <chuanhquoc2410@gmail.com>
2024-10-20 08:04:11 +0700

    update ForgotPassword

--- src/main/java/view/controller/ForgotPasswordController.java
+++ src/main/java/view/controller/ForgotPasswordController.java
@@ -1,28 +1,14 @@
-// src/main/java/com/example/LoginController.java
 package view.controller;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
 
-public class LoginController {
-
-    @FXML
-    private void handleSignUpAction(ActionEvent event) throws IOException {
-        Parent signUpParent = FXMLLoader.load(getClass().getResource("/fxml/SignUp.fxml"));
-        Scene signUpScene = new Scene(signUpParent);
-
-        // Get the stage information
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(signUpScene);
-        window.show();
-    }
+public class ForgotPasswordController {
 
     public void handleForgotPasswordAction(ActionEvent event) {
         try {
@@ -40,5 +26,4 @@ public class LoginController {
             e.printStackTrace();
         }
     }
-
 }
