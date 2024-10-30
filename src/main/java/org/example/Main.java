86546eb8a70549c0ab0bc08c56b773a1dd4b384b
lgthvinh <everwellmax@gmail.com>
2024-10-30 11:49:20 +0700

    test AuthController
    Signed-off-by: lgthvinh <everwellmax@gmail.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import com.lims.controller.AuthenticationController;
+
 public class Main {
+
+    static AuthenticationController authController = new AuthenticationController();
+
     public static void main(String[] args) {
         System.out.println("Data-core");
+        authController.authenticate("everwellmax@gmail.com", "123456");
+        if (authController.isAuthenticated()) {
+            System.out.println("Authenticated");
+        } else {
+            System.out.println("Not Authenticated");
+        }
+        authController.deleteAuthentication();
     }
 }
