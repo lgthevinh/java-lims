c2afeb410f2d5c4e36527dce0c229acec5ea1fc2
lgthvinh <everwellmax@gmail.com>
2024-11-10 17:04:04 +0700

    remove un-necessary
    Signed-off-by: lgthvinh <everwellmax@gmail.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,7 @@
 package org.example;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginView.fxml"));
-        primaryStage.setTitle("Library Management System");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        System.out.println("Data core");
     }
 }
