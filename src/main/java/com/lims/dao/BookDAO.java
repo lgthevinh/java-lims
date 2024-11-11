1db3d72899dfa89b6fc6a41ee0e04d082c62d708
lgthvinh <everwellmax@gmail.com>
2024-11-11 21:54:20 +0700

    update connection auto close when error occurs
    Signed-off-by: lgthvinh <everwellmax@gmail.com>

--- src/main/java/com/lims/dao/BookDAO.java
+++ src/main/java/com/lims/dao/BookDAO.java
@@ -18,83 +18,110 @@ public class BookDAO extends DatabaseManager {
     public static List<Book> getAllBooks() throws SQLException, ParseException {
         List<Book> books = new ArrayList<>();
         Connection conn = getConnection();
-        Statement statement = conn.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM Book");
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Book");
 
-        while (resultSet.next()) {
-            Book book = new Book(
-                    resultSet.getString("isbn"),
-                    resultSet.getString("title"),
-                    resultSet.getString("author"),
-                    convertStringToDatetime("MM-dd-yyyy", resultSet.getString("year_of_publication")),
-                    resultSet.getString("publisher"),
-                    resultSet.getString("image_url"),
-                    resultSet.getInt("available_amount")
-            );
-            books.add(book);
+            while (resultSet.next()) {
+                Book book = new Book(
+                        resultSet.getString("isbn"),
+                        resultSet.getString("title"),
+                        resultSet.getString("author"),
+                        convertStringToDatetime("MM-dd-yyyy", resultSet.getString("year_of_publication")),
+                        resultSet.getString("publisher"),
+                        resultSet.getString("image_url"),
+                        resultSet.getInt("available_amount")
+                );
+                books.add(book);
+            }
+            conn.close();
+            return books;
+        } catch (SQLException | ParseException e) {
+            conn.close();
+            throw e;
         }
-        conn.close();
-        return books;
+
     }
 
     public static Book getBookByISBN(String isbn) throws SQLException, ParseException {
         Connection conn = getConnection();
-        Statement statement = conn.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM Book WHERE isbn = '%s'".formatted(isbn));
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Book WHERE isbn = '%s'".formatted(isbn));
 
-        if (resultSet.next()) {
-            Book book = new Book(
-                    resultSet.getString("isbn"),
-                    resultSet.getString("title"),
-                    resultSet.getString("author"),
-                    convertStringToDatetime("MM-dd-yyyy", resultSet.getString("year_of_publication")),
-                    resultSet.getString("publisher"),
-                    resultSet.getString("image_url"),
-                    resultSet.getInt("available_amount")
-            );
+            if (resultSet.next()) {
+                Book book = new Book(
+                        resultSet.getString("isbn"),
+                        resultSet.getString("title"),
+                        resultSet.getString("author"),
+                        convertStringToDatetime("MM-dd-yyyy", resultSet.getString("year_of_publication")),
+                        resultSet.getString("publisher"),
+                        resultSet.getString("image_url"),
+                        resultSet.getInt("available_amount")
+                );
+                conn.close();
+                return book;
+            }
+
             conn.close();
-            return book;
+            return null;
+        } catch (SQLException | ParseException e) {
+            conn.close();
+            throw e;
         }
-
-        conn.close();
-        return null;
     }
 
     public static void addBookToDatabase(Book book) throws SQLException {
         Connection conn = getConnection();
-        Statement statement = conn.createStatement();
-        statement.executeUpdate("INSERT INTO Book VALUES ('%s', '%s', '%s', '%s', '%s', '%s', %d)".formatted(
-                book.getIsbn(),
-                book.getTitle(),
-                book.getAuthor(),
-                formatDatetime("MM-dd-yyyy", book.getYearOfPublication()),
-                book.getPublisher(),
-                book.getImageUrl(),
-                book.getAvailableAmount()
-        ));
-        conn.close();
+        try {
+            Statement statement = conn.createStatement();
+            statement.executeUpdate("INSERT INTO Book VALUES ('%s', '%s', '%s', '%s', '%s', '%s', %d)".formatted(
+                    book.getIsbn(),
+                    book.getTitle(),
+                    book.getAuthor(),
+                    formatDatetime("MM-dd-yyyy", book.getYearOfPublication()),
+                    book.getPublisher(),
+                    book.getImageUrl(),
+                    book.getAvailableAmount()
+            ));
+            conn.close();
+        } catch (SQLException e) {
+            conn.close();
+            throw e;
+        }
+
     }
 
     public static void deleteBookFromDatabase(String book_isbn) throws SQLException {
         Connection conn = getConnection();
-        Statement statement = conn.createStatement();
-        statement.executeUpdate("DELETE FROM Book WHERE isbn = '%s'".formatted(book_isbn));
-        conn.close();
+        try {
+            Statement statement = conn.createStatement();
+            statement.executeUpdate("DELETE FROM Book WHERE isbn = '%s'".formatted(book_isbn));
+            conn.close();
+        } catch (SQLException e) {
+            conn.close();
+            throw e;
+        }
+
     }
 
     public static void updateBookInDatabase(Book book) throws SQLException {
         Connection conn = getConnection();
-        Statement statement = conn.createStatement();
-        statement.executeUpdate("UPDATE Book SET title = '%s', author = '%s', year_of_publication = '%s', publisher = '%s', image_url = '%s', available_amount = '%d' WHERE isbn = '%s'".formatted(
-                book.getTitle(),
-                book.getAuthor(),
-                formatDatetime("MM-dd-yyyy", book.getYearOfPublication()),
-                book.getPublisher(),
-                book.getImageUrl(),
-                book.getAvailableAmount(),
-                book.getIsbn()
-        ));
-        conn.close();
+        try {
+            Statement statement = conn.createStatement();
+            statement.executeUpdate("UPDATE Book SET title = '%s', author = '%s', year_of_publication = '%s', publisher = '%s', image_url = '%s', available_amount = '%d' WHERE isbn = '%s'".formatted(
+                    book.getTitle(),
+                    book.getAuthor(),
+                    formatDatetime("MM-dd-yyyy", book.getYearOfPublication()),
+                    book.getPublisher(),
+                    book.getImageUrl(),
+                    book.getAvailableAmount(),
+                    book.getIsbn()
+            ));
+            conn.close();
+        } catch (SQLException e) {
+            conn.close();
+        }
     }
 
 }
