e66089325b2a880b089bfd2d415edb142a3bc1bf
lgthvinh <everwellmax@gmail.com>
2024-11-13 11:41:05 +0700

    example
    Signed-off-by: lgthvinh <everwellmax@gmail.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import com.lims.controller.AuthenticationController;
+import com.lims.controller.AuthorizationController;
+
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Data core");
+
+        AuthenticationController.deleteAuthentication();
+
+        if (!AuthenticationController.isAuthenticated()) {
+            AuthenticationController.authenticate("trinhngocthong@gmail.com", "tnt123");
+        }
+
+        try {
+            System.out.println(AuthorizationController.isAuthorized(0) ? "Admin" : "User");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
